f0f4bccc9dd7fb8b9306583b0d2c3a35d793fb24
Ashok Bhobhiya <ashokbhobhiya.181cv107@nitk.edu.in>
2020-06-01 00:52:36 +0530

    Update connectivity_by_dfs.py
    Added delete_edge and modified is_connected function

--- connectivity_by_dfs.py
+++ connectivity_by_dfs.py
@@ -9,24 +9,40 @@ class Graph:
     def __init__(self): 
         self.adList = defaultdict(list)
         self.vertex=[]
-        self.visited=[None]*100
+        self.visited=[None]*1000
         self.length=0
 
-    def addVertex(self,u):
+    def add_vertex(self,u):
+        if u in self.vertex:
+            return True
         self.vertex.append(u)
         self.visited[u]=False
         self.length = self.length+1
 
-    def cutVertex(self,u):
+    def delete_vertex(self,u):
         self.vertex.remove(u)
-        for i in range(len(self.adList)):
+        for i in self.adList:
             if u in self.adList[i]:
                 self.adList[i].remove(u)
         del self.adList[u]
+        return "Vertex ("+str(u)  + ") successfully deleted."
 
-    def addEdge(self,u,v):
+    def add_edge(self,u,v):
+        if v in self.adList[u] and u in self.adList[v]:
+            return "Edge ("+str(u) +","+ str(v) + ") already there."
         self.adList[u].append(v)
         self.adList[v].append(u)
+        return "Edge ("+str(u) +","+ str(v) + ") successfully added."
+
+    def delete_edge(self,u,v):
+        if v in self.adList[u]:
+            self.adList[u].remove(v)
+            self.adList[v].remove(u)
+            return "Edge ("+str(u) +","+ str(v) + ") successfully deleted."
+        else:
+            return "Edge ("+str(u) +","+ str(v) + ") isn't a direct edge or may be already deleted."
+
+
 
     def dfs(self,v):
         # print(self.visited)
@@ -39,51 +55,66 @@ class Graph:
                 self.dfs(u)
 
 
-    def isConnected(self):
+    def is_connected(self,u,v):
         # print(self.length)
-        self.visited=[False]*self.length
-        self.dfs(self.vertex[0])
+        # self.visited=[False]*(self.length)
+        if v in self.adList[u]:
+            return "Edge, ("+str(u) +","+ str(v) + ") connected"
+        self.visited=[False]*1000
+        self.dfs(u)
         # print(self.visited)
-        for i in ((self.adList)):
-            # print("yes")
-            if self.visited[i] == False:
-                # print("yeahhhh...")
-                return False
-        return True
+        # for i in ((self.adList)):
+        #     # print("yes")
+        #     if self.visited[i] == False:
+        #         # print("yeahhhh...")
+        #         return False
+        # return True
+        if self.visited[v] == True:
+            return "Edge, ("+str(u) +","+ str(v) + ") connected" 
+        else:
+            return "Edge, ("+str(u) +","+ str(v) + ") not connected" 
+
+
+
 # Driver code 
 g = Graph()
-g.addVertex(0)
-g.addVertex(1)
-g.addVertex(2) 
-g.addVertex(3)
-g.addVertex(4)
-g.addVertex(5)
-g.addEdge(0, 2) 
-g.addEdge(0, 3)
-g.addEdge(0, 5)
-g.addEdge(1, 3)  
-g.addEdge(1, 5)
-g.addEdge(2, 4)
-print(g.isConnected())
-# print(g.adList)
-# print(len(g.adList))
-g.cutVertex(0)
-# print(len(g.adList))
-# print(g.adList)
-print(g.isConnected())
-# g.addVertex(4)
-# g.addEdge(2,4)
-# g.addEdge(2,0)
-# print(g.isConnected())
-# g.cutVertex(4)
-# print(g.isConnected())
-# g.cutVertex(4)
-# g.addVertex(4)
-# g.addEdge(2, 4)
-# g.cutVertex(0)
-# print(g.adList)
-# print(g.vertex)
-# print(g.adList)
-  
-# print ("Following is DFS traversal")
-# g.dfs(2) 
+g.add_vertex(2)
+g.add_vertex(25)
+g.add_vertex(100)
+g.add_vertex(900)
+g.add_vertex(889)
+g.add_vertex(111)
+g.add_vertex(1)
+g.add_vertex(15)
+g.add_vertex(99)
+g.add_edge(2,25)
+g.add_edge(889,15)
+g.add_edge(111,15)
+g.add_edge(100,111)
+g.add_edge(100,25)
+g.add_edge(2,900)
+g.add_edge(889,25)
+g.add_edge(1,2)
+g.add_edge(1,99)
+
+print(g.is_connected(99,15))
+print(g.delete_edge(2,25))
+# print(g.delete_edge(2,25))
+print(g.is_connected(99,15))
+print(g.add_edge(2,25))
+print(g.is_connected(99,15))
+print(g.delete_edge(889,25))
+print(g.delete_edge(100,111))
+print(g.is_connected(99,15))
+print(g.delete_vertex(25))
+print(g.is_connected(100,25))
+print(g.is_connected(99,100))
+print(g.add_edge(889,100))
+print(g.is_connected(99,15))
+g.add_vertex(112)
+print(g.add_edge(111,112))
+print(g.is_connected(1,112))
+print(g.adList)
+print(g.vertex)
+
+
